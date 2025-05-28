040617f8bda02bde84bc14dd9fd79b7dfe55bc4c
PhantomXNaveen <yadavnyny77@gmail.com>
2025-05-28 06:55:57 +0530

    deployment phase4

--- app.py
+++ app.py
@@ -36,5 +36,8 @@ with app.app_context():
  
 excel.init_excel(app)
 
-if __name__ == '__main__':
-    app.run()
+# if __name__ == '__main__':
+#     app.run()
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5000)
